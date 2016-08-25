c51a22cb6f7c0c87399fe0546484e320d02b2606
Simon T Glancy <si.glancy@gmail.com>
2016-08-25 23:19:03 +0100

    updated routes

--- escapists/js/app.js
+++ escapists/js/app.js
@@ -6,27 +6,27 @@ function($httpProvider,$stateProvider, $urlRouterProvider) {
 
   $stateProvider.state('home', {
     url: '/home',
-    templateUrl: '/js/templates/home.html',
+    templateUrl: 'escapists/js/templates/home.html',
     controller: 'MainCtrl'
   });
   $stateProvider.state('look', {
     url: '/look',
-    templateUrl: '/js/templates/look.html',
+    templateUrl: 'escapists/js/templates/look.html',
     controller: 'LookCtrl'
   })
   $stateProvider.state('listen', {
     url: '/listen',
-    templateUrl: '/js/templates/listen.html',
+    templateUrl: 'escapists/js/templates/listen.html',
     controller: 'ListenCtrl'
   })
   $stateProvider.state('live', {
     url: '/live',
-    templateUrl: '/js/templates/live.html',
+    templateUrl: 'escapists/js/templates/live.html',
     controller: 'LiveCtrl'
   });
   $stateProvider.state('join', {
     url: '/join',
-    templateUrl: '/js/templates/join.html',
+    templateUrl: 'escapists/js/templates/join.html',
     controller: 'JoinCtrl'
   });
 
